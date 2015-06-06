f19c1a2d9ed6b54051d7e43056cfe7849a60d224
Bobby Cahill <bobby@bobbycahill.com>
2015-06-06 14:36:02 -0400

    Start with basic Laravel implementation

---
+++ env.dev.default.php
@@ -0,0 +1,25 @@
+<?php
+
+// Configuration for sensitive information in local environment
+//
+// FIRST-TIME Projectify installers, do the following:
+// 1) Copy this file from 'env.dev.default.php' to '.env.dev.php' (note the preceding dot)
+// 2) Change the values in the array to fit your database credentials
+// 3) Don't forget to create the database and user in the database itself
+
+return array(
+
+    'DB_HOST' => 'your-db-hostname',
+    'DB_NAME' => 'projectify-dev',
+    'DB_USER' => 'projectify-dev',
+    'DB_PASS' => 'password-for-projectify-dev-user',
+    'MAIL_PRETEND' => false,
+    'MAIL_HOST' => 'your.smtp.server.com',
+    'MAIL_PORT' => 465, // recommended
+    'MAIL_ENCRYPTION' => 'ssl', // recommended
+    'MAIL_USER' => 'your-email-username',
+    'MAIL_PASS' => 'your-email-password',
+    'MAIL_DISPLAY_ADDRESS' => 'your.email.display.address@your.smtp.server.com',
+    'MAIL_DISPLAY_NAME' => 'Your Email Display Name'
+
+);
