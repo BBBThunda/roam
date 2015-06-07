ecd5673e430147eddc324a57ca36b286b0dd6ee1
= <jason.t.stillerman@gmail.com>
2015-06-06 22:22:39 -0400

    Talks to server

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,19 +1,17 @@
 console.log("Main Script initialized")
 
-
-
 function setup(){
 
   $(".target").click(function() {
     var idnumber =  $(this).attr("data-tour-id");
     console.log(idnumber);
   //  debugger;
-    $.post("signupForTour.php",
+    $.post("proxy.php?url=http://45.33.82.240:5000/signUpForTour/1",
     {
         name: idnumber
     },
     function(data, status){
-        alert(data);
+        alert(data)
     });
 
   });
